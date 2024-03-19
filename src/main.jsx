6656592db3dd141258dbb10e8b222eeedf29aba9
sammyklan3 <sammyklane7@gmail.com>
2024-03-19 17:07:59 +0300

    Adding error file

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './index.css';
 import ReactDOM from 'react-dom/client';
 import { Home } from './pages/home/Home';
+import { Error } from './pages/error/Error';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -12,6 +13,11 @@ const router = createBrowserRouter([
     path: "/",
     element: (<Home />),
   },
+
+  {
+    path: "*",
+    element: (<Error />),
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ src/pages/error/Error.jsx
@@ -0,0 +1,11 @@
+import "./error.css";
+
+export const Error = () => {
+  return (
+    <div className="error-container">
+      <h2>Oops!</h2>
+        <p>An unexpected error has occured.</p>
+        <small><i>Not Found</i></small>
+    </div>
+  )
+}
